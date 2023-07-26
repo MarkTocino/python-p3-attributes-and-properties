a7d9c8c3d55c0c056aca9081c1c15fc8b4f9357b
MarkTocino <Mark_tocino@yahoo.com>
2023-07-26 16:17:02 -0400

    Completed Dog Person attributes properties

--- lib/dog.py
+++ lib/dog.py
@@ -10,6 +10,31 @@ APPROVED_BREEDS = [
     "Pug",
     "Pointer"
 ]
-
+import ipdb
 class Dog:
+    def __init__(self, name= "Fido", breed = "Corgi"):
+        self.name = name
+        self.breed = breed
+
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, name): 
+        if name == "" or type(name) != str or len(name) > 25:
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self._name = name
+
+    name = property(get_name, set_name)
+
+    def get_breed(self):
+        return self._breed
+    
+    def set_breed(self, breed):
+        if breed in APPROVED_BREEDS:
+            self._breed = breed
+        else:
+            print("Breed must be in list of approved breeds.")
+
+    breed = property(get_breed, set_breed)
     pass
